bb0e6eb9433412433a2167d91d752eaa7dae0cd6
Jackson <jacksonludwig74@yahoo.com>
2019-12-26 12:39:02 -0500

    add 10 percent chance to get random pasta

--- index.js
+++ index.js
@@ -8,14 +8,30 @@ const PREFIX = "!";
 const file_system = require('fs');
 const image_dir = './images/';
 
+const NUMBER_OF_PASTAS = 2;
+
 client.login(TOKEN);
 
 client.on("ready", () => {
     console.log("This bot is online");
 })
 
-client.on("message", message => {
+function getPasta(type) {
+    var jsonString = file_system.readFileSync("copypastas.json", "utf8");
+    var obj = JSON.parse(jsonString);
+    switch (type) {
+        case "furry":
+            var random_array_value = Math.floor(Math.random() * obj.furry_pastas.length);
+            return obj.furry_pastas[random_array_value].pasta;
+        case "weeb":
+            var random_array_value = Math.floor(Math.random() * obj.weeb_pastas.length);
+            return obj.weeb_pastas[random_array_value].pasta;
+        default:
+            break;
+    }
+}
 
+client.on("message", message => {
     if (message.content.includes("loli")) {
         message.reply("Ahh a fewwow cutie I see UwU~~");
     } else if (message.content.substring(0, 1) === "!") {
@@ -31,18 +47,12 @@ client.on("message", message => {
                 });
                 break;
             case "furry":
-                var jsonString = file_system.readFileSync("copypastas.json", "utf8");
-                var obj = JSON.parse(jsonString);
-
-                var random_array_value = Math.floor(Math.random() * obj.furry_pastas.length);
-                message.channel.send(obj.furry_pastas[random_array_value].pasta);
+                var pasta = getPasta("furry");
+                message.channel.send(pasta);
                 break;
             case "weeb":
-                var jsonString = file_system.readFileSync("copypastas.json", "utf8");
-                var obj = JSON.parse(jsonString);
-                
-                var random_array_value = Math.floor(Math.random() * obj.weeb_pastas.length);
-                message.channel.send(obj.weeb_pastas[random_array_value].pasta);
+                var pasta = getPasta("weeb");
+                message.channel.send(pasta);
                 break;
             default:
                 break;
@@ -51,7 +61,7 @@ client.on("message", message => {
         var chance_for_random_message = Math.random();
 
         if (message.member.user.tag != "DegenerateBot#4865"
-            && chance_for_random_message >= 0 
+            && chance_for_random_message >= 0
             && chance_for_random_message <= .25) {
 
             var smileys = [";;w;;", "^w^", ">w<", "UwU", "(・`ω\´・)", "(´・ω・\`)"];
@@ -62,6 +72,23 @@ client.on("message", message => {
 
             owo_msg += " " + random_smiley;
             message.reply(owo_msg);
+        } else if (message.member.user.tag != "DegenerateBot#4865"
+            && chance_for_random_message > .25
+            && chance_for_random_message <= .35) {
+            message.reply("WOW~ YOU GOT A RARE PWASTA DROP UWU!");
+            var randomPastaType = Math.floor(Math.random() * NUMBER_OF_PASTAS);
+            switch (randomPastaType) {
+                case 0:
+                    var pasta = getPasta("furry")
+                    message.channel.send(pasta);
+                    break;
+                case 1:
+                    var pasta = getPasta("weeb")
+                    message.channel.send(pasta);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 })
