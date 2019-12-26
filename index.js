c6d7713ac7d310207f747371b85be7eaa0349ce5
Jackson <jacksonludwig74@yahoo.com>
2019-12-26 01:24:43 -0500

    random image command expands to any number of images

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const Discord = require('discord.js')
 const client = new Discord.Client();
 
-const token = "NjU5NjE3MzA3MTAxNzU3NDQw.XgQ7Dw.4HFb2XiQpr42w20VXtYh2VwR-tQ";
+const TOKEN = "NjU5NjE3MzA3MTAxNzU3NDQw.XgQ7Dw.4HFb2XiQpr42w20VXtYh2VwR-tQ";
 
-client.login(token)
+const PREFIX = "!";
+
+const file_system = require('fs');
+const image_dir = './images/';
+
+client.login(TOKEN)
 
 client.on("ready", () => {
     console.log("This bot is online");
 })
 
 client.on("message", message => {
-    if(message.content === "HELLO") {
-        message.reply("SUP DEGEN");
+    if(message.content.includes("loli")) {
+        message.reply("Ahh a fewwow cutie I see UwU~~");
+    } else {
+        let args = message.content.substring(PREFIX.length).split(" ");
+
+        switch(args[0]) {
+            case "degen":
+                file_system.readdir(image_dir, (err, files) => {
+                    numberOfImages = files.length;
+                    message.reply("OwO IF YOU SAY SO~~~");
+                    randomImage = Math.floor (Math.random() * numberOfImages) + 1;
+                    message.channel.send ({files: [image_dir + randomImage + ".png"]})
+                  });
+                break;
+    
+            default:
+                break;
+        }
     }
 })
